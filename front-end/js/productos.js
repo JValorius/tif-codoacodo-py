6dd66306650216cf9b780780212f5a6326801088
JValorius <juan.m.vallejo.m@gmail.com>
2023-07-01 19:06:41 -0300

    Redireccionado a mi API

--- front-end/js/productos.js
+++ front-end/js/productos.js
@@ -8,7 +8,7 @@ createApp({
     return {
       productos: [], // Almacena los productos obtenidos del backend
       // url:'http://localhost:5000/productos', // URL local
-      url: "https://ramirocodoacodo.pythonanywhere.com/productos", // URL del backend donde se encuentran los productos
+      url: "https://jvalorius.pythonanywhere.com/productos", // URL del backend donde se encuentran los productos
       error: false,
       cargando: true,
       // Atributos para el almacenar los valores del formulario
