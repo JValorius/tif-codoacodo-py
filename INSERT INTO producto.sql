4842c9e69d62b245988dae211ab67bf6e3b74669
JValorius <juan.m.vallejo.m@gmail.com>
2023-07-01 18:24:52 -0300

    Primera carga

---
+++ INSERT INTO producto.sql
@@ -0,0 +1,10 @@
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('iPhone 12', 1200, 50, 'iphone12.jpg');
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('Samsung Galaxy S21', 1100, 40, 'galaxyS21.jpg');
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('Sony PlayStation 5', 500, 30, 'ps5.jpg');
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('Apple AirPods Pro', 250, 100, 'airpodsPro.jpg');
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('Samsung 4K Smart TV', 800, 20, 'samsungTV.jpg');
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('Canon EOS Rebel T7i', 700, 15, 'canonCamera.jpg');
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('Nintendo Switch', 300, 60, 'nintendoSwitch.jpg');
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('HP Spectre x360', 1500, 10, 'hpLaptop.jpg');
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('Fitbit Versa 3', 200, 80, 'fitbitVersa.jpg');
+INSERT INTO producto (nombre, precio, stock, imagen) VALUES ('Bose QuietComfort 35 II', 350, 25, 'boseHeadphones.jpg');
